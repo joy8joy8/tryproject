a2805fe1e27b1af397a5814447d42731022976ee
USER <USER@LAPTOP-NP70I9FR>
2018-09-30 22:43:09 +0800

    night

--- tryproject/src/com/lotte2/Trymany.java
+++ tryproject/src/com/lotte2/Trymany.java
@@ -4,7 +4,12 @@ public class Trymany {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print(" wish you another 120 happy years\nthe time now is ");
+		System.out.print(" wish you another 120 happy years");
+		int second= 50;
+		System.out.println(second/100);
+		double min=77.0;
+		System.out.println(min/100);
+		System.out.println(min/78);
 	}
 
 }
