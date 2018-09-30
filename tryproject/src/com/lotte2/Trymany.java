3fc49e88b57dcba81347a7c12b91ccf056344870
USER <USER@LAPTOP-NP70I9FR>
2018-09-30 22:30:36 +0800

    try

---
+++ tryproject/src/com/lotte2/Trymany.java
@@ -0,0 +1,10 @@
+package com.lotte2;
+
+public class Trymany {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(" wish you another 120 happy years\nthe time now is ");
+	}
+
+}
