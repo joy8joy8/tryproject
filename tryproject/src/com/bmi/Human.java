874f5e0faa6383bffc4ea997576a3ddff32f2105
USER <USER@LAPTOP-NP70I9FR>
2018-10-08 21:30:24 +0800

    wrong

---
+++ tryproject/src/com/bmi/Human.java
@@ -0,0 +1,23 @@
+package com.bmi;
+
+public class Human {
+	float weight;
+	float height;
+	float bmi=weight/(height*height);
+public static void bmi(); 
+}
+
+
+/*Scanner sc =new Scanner(System.in); 
+Scanner sc2 =new Scanner(System.in); 
+String [] name=new String[5]; 
+String [] Subject={"國文","英文"}; 
+int [] chScore=new int[5]; 
+int [] engScore=new int[5]; 
+double chAvg=0.0D,engAvg=0.0D; 
+double[] avg = new double[5]; 
+int[]sum=new int[5]; 
+int i; 
+System.out.println("請輸入姓名"); 
+for(i=0; i<5 ;i++){ 
+name[i] =sc.next(); */
